3e6547a260371cacca927e0c1ffcf382bfbade0f
vishwateja77 <vishwat0039@gmail.com>
2023-07-24 09:21:12 +0000

    authentication 2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,28 @@
+import {Redirect} from 'react-router-dom'
+import Cookies from 'js-cookie'
+
+import Header from '../Header'
+
+import './index.css'
+
+const Cart = () => {
+  const accessToken = Cookies.get('jwt-token')
+  if (accessToken === undefined) {
+    return <Redirect to="/login" />
+  }
+
+  return (
+    <>
+      <Header />
+      <div className="cart-container">
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+          alt="cart"
+          className="cart-img"
+        />
+      </div>
+    </>
+  )
+}
+
+export default Cart
